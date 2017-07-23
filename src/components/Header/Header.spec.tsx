858e4217408ec2255d3494b0e48c8d34de5e7de7
Llorenç Muntaner <llorenc.muntaner@gmail.com>
2017-07-23 15:16:24 +0200

    Style issues

--- src/components/Header/Header.spec.tsx
+++ src/components/Header/Header.spec.tsx
@@ -1,10 +1,9 @@
-import React from 'react';
 import { shallow } from 'enzyme';
+import React from 'react';
 
 import Header from './index';
 
 test('jest works fine', () => {
   const component = shallow(<Header title="Testing" />);
   expect(component).toMatchSnapshot();
-  // expect(true).toBeTruthy();
 });
